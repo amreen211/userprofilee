26dfc4ee1b7e53eb8b4aff1853473ce63a531f75
amreen211 <amreenkhatik@gmail.com>
2023-04-13 11:47:43 +0530

    helo

--- src/App.js
+++ src/App.js
@@ -22,4 +22,53 @@ function App() {
   );
 }
 
+  return (
+    <div className="container">
+      <h1>User Profiles</h1>
+      <div className="user-list">
+        {users.map(user => (
+          <div key={user.id} className="user-card">
+            <img src={generateAvatarUrl(user.username)} alt={user.name} />
+            <h2>{user.name}</h2>
+            <p><FaEnvelope /> {user.email}</p>
+            <p><FaPhoneAlt /> {user.phone}</p>
+            <p>{user.address.street}, {user.address.suite}, {user.address.city}</p>
+            <p><FaGlobe /> {user.website}</p>
+            <p>{user.company.name}</p>
+
+            <div className="user-actions">
+              {likedUserIds.includes(user.id)
+                ? <FaHeart className="heart-icon red" onClick={() => handleUnlikeUser(user.id)} />
+                : <FaHeart className="heart-icon" onClick={() => handleLikeUser(user.id)} />}
+              <button onClick={() => setEditingUserId(user.id)}><FaEdit /></button>
+              <FaTrash />
+            </div>
+
+            {editingUserId === user.id &&
+              <div className="popup">
+                <form onSubmit={handleFormSubmit}>
+                  
+                  <label htmlFor="name">Name:</label>
+                  <input type="text" name="name" value={formData.name} onChange={handleInputChange} required />
+
+                  <label htmlFor="email">Email:</label>
+                  <input type="email" name="email" value={formData.email} onChange={handleInputChange} required />
+
+                  <label htmlFor="phone">Phone:</label>
+                  <input type="tel" name="phone" value={formData.phone} onChange={handleInputChange} required />
+
+                  <label htmlFor="website">Website:</label>
+                  <input type="url" name="website" value={formData.website} onChange={handleInputChange} required />
+                  <button type="submit">Save</button>
+                  <button type="button" onClick={handleCancel}>Cancel</button>
+
+</form>
+</div>
+}
+</div>
+))}
+</div>
+</div>
+);
+}
 export default App;
